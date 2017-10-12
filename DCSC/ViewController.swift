c7442eade4a4c8c82c2919b05f333fbb7a0a158f
Andrew Cope <andrew@thecope.net>
2017-10-11 21:28:30 -0700

    Initial Commit

---
+++ DCSC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DCSC
+//
+//  Created by Andrew Cope on 10/1/17.
+//  Copyright © 2017 Andrew Cope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("this is so fun!!!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
